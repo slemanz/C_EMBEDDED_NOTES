f298d1daaf8b1105952fe6bae05ff1fe760f3668
slemanz <william_sleman@hotmail.com>
2023-10-07 23:39:18 -0300

    update

--- 1_FUNDAMENTALS/01_variables.c
+++ 1_FUNDAMENTALS/01_variables.c
@@ -1,7 +1,20 @@
+/************************************************
+*                                               *
+*           DATA TYPES AND VARIABLES            *
+*                                               *
+*************************************************/
 #include <stdio.h>
+
 
 void main()
 {
-	printf("Test\n");
-	printf("Test\n");
+	// In C we have TWO signicant data types:
+	//   |----Integer and Float
+
+	char var_char = 'C'; // consumes 1 byte of memory always
+	unsigned char var_char2; // exemple of initialization
+	var_char2 = 67;
+
+	printf("Char Format: %c \n", var_char2);
+	printf("Intenger Format: %d \n", var_char2); // Could be also %u for unsigned
 }
