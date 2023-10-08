b6c3de34c0b549206b0b6eaded57b5bebe0fe977
slemanz <william_sleman@hotmail.com>
2023-10-08 00:14:01 -0300

    Makefile added

--- 1_FUNDAMENTALS/01_variables.c
+++ 1_FUNDAMENTALS/01_variables.c
@@ -17,4 +17,12 @@ void main()
 
 	printf("Char Format: %c \n", var_char2);
 	printf("Intenger Format: %d \n", var_char2); // Could be also %u for unsigned
+
+	// sizeof operator (compiler dependent)
+	printf("\nSize of char data type: %ld\n", sizeof(char));
+	printf("Size of short data type: %ld\n", sizeof(short));
+	printf("Size of int data type: %ld\n", sizeof(int));
+	printf("Size of long data type: %ld\n", sizeof(long));
+	printf("Size of long long data type: %ld\n\n", sizeof(long long));
+
 }
