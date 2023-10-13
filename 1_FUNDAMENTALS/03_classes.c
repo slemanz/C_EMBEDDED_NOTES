25b1c43968e8e666fc9c95b7bef5a5c2c77c3eeb
slemanz <william_sleman@hotmail.com>
2023-10-13 05:23:48 -0300

    fundamental(feat): explain extern functionality
    the extern classe was explained.

--- 1_FUNDAMENTALS/03_classes.c
+++ 1_FUNDAMENTALS/03_classes.c
@@ -5,7 +5,33 @@
 ***********************************************************************************/
 #include <stdio.h>
 
+void myFunc1(void);
+int mainGlobalData;
+void file_Func1();
+
 void main()
 {
-	printf("Hello world\n");
+	// Storage Classes decide:
+	// The scope, visibility and life time of a variable.
+	// static and extern
+
+	myFunc1();
+	myFunc1();
+	myFunc1();
+	myFunc1();
+
+	mainGlobalData = 100;
+	printf("mainGlobalData = %d\n", mainGlobalData);
+	file_Func1();
+	printf("mainGlobalData = %d\n", mainGlobalData);
+
+}
+
+void myFunc1(void)
+{
+	// a global variable that is private to a function.
+	// not lose its existence 
+	static int count = 0;
+	count = count + 1;
+	printf("This function was executed %d times\n", count);
 }
