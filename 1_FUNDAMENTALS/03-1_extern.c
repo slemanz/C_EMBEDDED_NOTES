cbf81f3facec33d8af1497b6e22c154a984e0a4e
slemanz <william_sleman@hotmail.com>
2023-10-13 05:32:20 -0300

    fundamentals(update): update some files names
    was update some files name to better sort of files

---
+++ 1_FUNDAMENTALS/03-1_extern.c
@@ -0,0 +1,12 @@
+/**********************************************************************************
+*                                                                                 *
+*                                    EXTERNAL                                     *
+*                                                                                 *
+***********************************************************************************/
+
+extern int mainGlobalData; // declare that variable exists
+
+void file_Func1(void)
+{
+	mainGlobalData = 200;
+}funcionalidade
